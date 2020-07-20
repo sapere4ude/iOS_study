ddb838a50817db238a28a01df5eb14b205cab0b2
sapere4ude <48309864+sapere4ude@users.noreply.github.com>
2020-07-20 15:29:21 +0900

    CloneScrollView2
    Content Layout & Frame Layout 을 사용한 Scroll View 만들기

---
+++ CloneScrollView2/CloneScrollView2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloneScrollView2
+//
+//  Created by sapere4ude on 2020/07/20.
+//  Copyright © 2020 sapere4ude. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
