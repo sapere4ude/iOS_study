26c5597490c6318be2cb8aeef1a76b72e587c917
eileenyoo2 <eileenyoo2@gmail.com>
2020-07-26 00:12:16 +0900

    Weather App

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by sapere4ude on 2020/07/25.
+//  Copyright © 2020 sapere4ude. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
