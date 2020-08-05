e76d99a859f4b0e3973638693a63f14972554f79
eileenyoo2 <eileenyoo2@gmail.com>
2020-08-05 21:39:53 +0900

    pod 추가, IBOulet 작업

--- Hwahae/Hwahae/Controller/HomeViewController.swift
+++ Hwahae/Hwahae/Controller/HomeViewController.swift
@@ -10,21 +10,45 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    // 제일 상단부
+    @IBOutlet var btnMainView: UIBarButtonItem!
+    @IBOutlet var serchProduct: UITextField!
+    @IBOutlet var btnShoppingView: UIBarButtonItem!
+    
+    // 랭킹, 화해쇼핑, 카테고리검색, 성분으로 검색
+    @IBOutlet var btnShowLanking: UIButton!
+    @IBOutlet var btnShopping: UIButton!
+    @IBOutlet var btnSearchCategory: UIButton!
+    @IBOutlet var btnSearchIngredients: UIButton!
+    
+    // 브랜드별 제품 이미지
+    @IBOutlet var productImgLeft: UIImageView!
+    @IBOutlet var productImgCenter: UIImageView!
+    @IBOutlet var productImgRight: UIImageView!
+    
+    // 브랜드 이름
+    @IBOutlet var brandNameLeft: UILabel!
+    @IBOutlet var brandNameCenter: UILabel!
+    @IBOutlet var brandNameRight: UILabel!
+    
+    // 제품 이름
+    @IBOutlet var productNameLeft: UILabel!
+    @IBOutlet var productNameCenter: UILabel!
+    @IBOutlet var productNameRight: UILabel!
+    
+    // 인기 신제품
+    @IBOutlet var newProduct1: UIImageView!
+    @IBOutlet var newProduct2: UIImageView!
+    @IBOutlet var newProduct3: UIImageView!
+    @IBOutlet var newProduct4: UIImageView!
+    @IBOutlet var newProduct5: UIImageView!
+    @IBOutlet var newProduct6: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Hwahae/Pods/Target Support Files/Pods-Hwahae/Pods-Hwahae-frameworks.sh
+++ Hwahae/Pods/Target Support Files/Pods-Hwahae/Pods-Hwahae-frameworks.sh
@@ -198,6 +198,8 @@ fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FSPagerView/FSPagerView.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromisesObjC/FBLPromises.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
@@ -208,6 +210,8 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FSPagerView/FSPagerView.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromisesObjC/FBLPromises.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
